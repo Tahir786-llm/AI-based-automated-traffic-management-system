be3cd766e49556c268e43a3eeb24139d9f3a652d
Sk MD Tahir Hossain <tahirhossain18012003@gmail.com>
2025-03-26 22:03:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 async function getTrafficData() {
-    const query = `{
-        trafficStatus(id: 1) {
-            location
-            congestionLevel
-        }
-    }`;
+    const query = `{ trafficStatus(location: "Highway 24") { location congestionLevel } }`;
 
     const response = await fetch("http://localhost:5000/graphql", {
         method: "POST",
@@ -13,5 +8,6 @@ async function getTrafficData() {
     });
 
     const data = await response.json();
-    document.getElementById("result").innerText = `Location: ${data.data.trafficStatus.location}, Congestion: ${data.data.trafficStatus.congestionLevel}`;
+    document.getElementById("result").innerText = 
+        `📍 Location: ${data.data.trafficStatus.location}\n🚦 Congestion: ${data.data.trafficStatus.congestionLevel}`;
 }
